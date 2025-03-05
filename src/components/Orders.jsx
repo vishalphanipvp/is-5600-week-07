a605f4891eadc71a4a7baa47b65308a1462d973f
Vishal Phani Parapatla <vishalphani2604@gmail.com>
2025-03-05 05:57:37 +0000

    week7

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react'; // Removed the duplicate React import
 import { BASE_URL } from '../config';
 
 const Orders = () => {
-  const [orders, setOrders] = useState([]);
+  const [orders, setOrders] = useState([]); // Added state for orders
 
-  /**
-   * TODO
-   * 1. Create a `fetchOrders` function that retrieves all orders from the database
+  /** 
+   * 1. Created `fetchOrders` to fetch the orders from the backend
    * 2. Using the `useEffect` hook, update the existing `orders` state object when `fetchOrders` is complete
-   **/ 
+   **/
+  
+  useEffect(() => {
+    fetchOrders();
+  }, [])
 
+  const fetchOrders = () => {
+    fetch(`${BASE_URL}/orders`)
+      .then((res) => res.json())
+      .then((data) => {
+        setOrders(data);
+      })
+  }
 
   return (
     <div className="center mw7 ba mv4">
